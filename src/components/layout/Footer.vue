26338b4fb9ff17734bfd76585cb9f22dcafab2b1
Chaney <ch3n4y@gmail.com>
2021-09-23 16:16:49 +0800

    fix year

--- src/components/layout/Footer.vue
+++ src/components/layout/Footer.vue
@@ -1,21 +1,31 @@
 <template>
   <el-footer class="main-footer">
-    Copyright &copy; 2020 Chaney. All rights reserved.
+    Copyright &copy; {{ year }} Chaney. All rights reserved.
   </el-footer>
 </template>
 <script>
-  export default {
+export default {
+  data: function () {
+    return {
+      year: 2020,
+    };
+  },
+  methods: {},
+  mounted() {
+    let nowDate = new Date();
+    this.year = nowDate.getFullYear()
   }
+}
 </script>
 <style>
-  .main-footer {
-    -webkit-transition: -webkit-transform 0.3s ease-in-out, margin 0.3s ease-in-out;
-    -moz-transition: -moz-transform 0.3s ease-in-out, margin 0.3s ease-in-out;
-    -o-transition: -o-transform 0.3s ease-in-out, margin 0.3s ease-in-out;
-    transition: transform 0.3s ease-in-out, margin 0.3s ease-in-out;
-    text-align: center;
-    padding: 5px 15px;
-    color: #444;
-    width: 100%;
-  }
+.main-footer {
+  -webkit-transition: -webkit-transform 0.3s ease-in-out, margin 0.3s ease-in-out;
+  -moz-transition: -moz-transform 0.3s ease-in-out, margin 0.3s ease-in-out;
+  -o-transition: -o-transform 0.3s ease-in-out, margin 0.3s ease-in-out;
+  transition: transform 0.3s ease-in-out, margin 0.3s ease-in-out;
+  text-align: center;
+  padding: 5px 15px;
+  color: #444;
+  width: 100%;
+}
 </style>
